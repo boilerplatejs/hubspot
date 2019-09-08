7990fdc3bd42e3c03038952aabb3d8a0faff4ac5
Peter C. Romano <services@vitruvian.tech>
2019-09-08 12:19:40 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# BoilerplateJS™
+
+## Bundle Template
+
+Bundle template designed for use within the BoilerplateJS™ app development platform.
---
+++ components/index.js
@@ -0,0 +1,7 @@
+// Include platform specific namespaces:
+export * as layout from './layout';
+export * as pages from './pages';
+export * as sections from './sections';
+export * as headers from './headers';
+
+// Include bundle specific namespaces:
